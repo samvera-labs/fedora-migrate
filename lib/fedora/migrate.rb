5bd5e6a78135c4c3629fdb5b6d32fe7b398b0f03
Adam Wead <amsterdamos@gmail.com>
2014-10-14 14:37:54 -0400

    Initial empty gem

---
+++ lib/fedora/migrate.rb
@@ -0,0 +1,7 @@
+require "fedora/migrate/version"
+
+module Fedora
+  module Migrate
+    # Your code goes here...
+  end
+end
---
+++ lib/fedora/migrate/version.rb
@@ -0,0 +1,5 @@
+module Fedora
+  module Migrate
+    VERSION = "0.0.1"
+  end
+end
