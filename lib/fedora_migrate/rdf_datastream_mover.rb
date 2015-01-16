0e276d1140e8a115a1c7023abd37513e083acc48
Adam Wead <amsterdamos@gmail.com>
2015-01-15 23:02:48 -0500

    Removing unused code

--- lib/fedora_migrate/rdf_datastream_mover.rb
+++ lib/fedora_migrate/rdf_datastream_mover.rb
@@ -13,30 +13,5 @@ module FedoraMigrate
       target.resource << RDF::Reader.for(:ntriples).new(source.content.gsub(/<.+#{source.pid}>/,"<#{target.uri}>"))
     end
 
-    private
-
-    # Date modified has to be treated differently, otherwise it attempts to set the date to
-    # today as well as the original date. This results in a ActiveFedora::Constraint error
-    # because the term can only have one value.
-    def apply_term statement
-      if statement.predicate.path.match(/modified$/)
-        target.date_modified = Date.parse(statement.object.value) if target.respond_to?(:date_modified)
-      else
-        target.resource << statement 
-      end
-    end
-
-    # See projecthydra/active_fedora#540
-    # Forcibly setting each attribute's changed status to true
-    def force_attribute_change
-      target.class.delegated_attributes.keys.each do |term|
-        target.send(term+"_will_change!") unless blacklist.member?(term)
-      end
-    end
-
-    def blacklist
-      [ "date_uploaded", "date_modified" ]
-    end
-
   end
 end
