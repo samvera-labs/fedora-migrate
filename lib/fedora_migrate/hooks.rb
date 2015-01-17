e64a9298ba9570f19b76df6baa3089048f378bc5
Adam Wead <amsterdamos@gmail.com>
2015-01-17 15:10:09 -0500

    More explanation to Hooks

--- lib/fedora_migrate/hooks.rb
+++ lib/fedora_migrate/hooks.rb
@@ -1,21 +1,32 @@
+# Override this methods to perform additional actions before and after
+# migation of objects and datastreams.
+#
+# To do so, simply define a FedoraMigrate::Hooks module anywhere in
+# you application and substitute methods for the ones listed below
 module FedoraMigrate
   module Hooks
 
+    # Called from FedoraMigrate::ObjectMover
     def before_object_migration
     end
 
+    # Called from FedoraMigrate::ObjectMover
     def after_object_migration
     end
 
+    # Called from FedoraMigrate::RDFDatastreamMover
     def before_rdf_datastream_migration
     end
 
+    # Called from FedoraMigrate::RDFDatastreamMover
     def after_rdf_datastream_migration
     end
 
+    # Called from FedoraMigrate::DatastreamMover
     def before_datastream_migration
     end
 
+    # Called from FedoraMigrate::DatastreamMover
     def after_datastream_migration
     end
 
