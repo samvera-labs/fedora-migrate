3f6056ac609babc6a699b631a62050aca4bb9c3e
Adam Wead <amsterdamos@gmail.com>
2015-02-04 15:56:49 -0500

    Omit missing relationships
    Instead of attempting--and failing--to migrate relationships for
    non-existent objects, just migrate the existing ones and omit the
    others, making a note in the log.

--- lib/fedora_migrate/rels_ext_datastream_mover.rb
+++ lib/fedora_migrate/rels_ext_datastream_mover.rb
@@ -4,26 +4,32 @@ module FedoraMigrate
     RELS_EXT_DATASTREAM = "RELS-EXT".freeze
 
     def migrate
-      statements.each do |statement|
-        subject.ldp_source.graph << [subject.rdf_subject, migrate_predicate(statement.predicate), migrate_object(statement.object)]
-      end
-      subject.ldp_source.update
+      migrate_statements
+      target.ldp_source.update
       update_index
+    end
+
+    def post_initialize
+      @target ||= ActiveFedora::Base.find(source.pid.split(/:/).last)
+    rescue ActiveFedora::ObjectNotFoundError
+      raise FedoraMigrate::Errors::MigrationError, "Target object was not found in Fedora4. Did you migrated it?"
     end
 
     private
 
+    def migrate_statements
+      statements.each do |statement|
+        target.ldp_source.graph << [target.rdf_subject, migrate_predicate(statement.predicate), migrate_object(statement.object)]
+      end
+    end
+
     def update_index
-      subject.reload
-      subject.update_index
+      target.reload
+      target.update_index
     end
 
     def graph
-      @graph ||= RDF::Graph.new { |g| g.from_rdfxml(rels_ext_content) }
-    end
-
-    def rels_ext_content
-      source.datastreams[RELS_EXT_DATASTREAM].content
+      @graph ||= RDF::Graph.new { |g| g.from_rdfxml(source.datastreams[RELS_EXT_DATASTREAM].content) }
     end
 
     # Override this if any predicate transformation is needed
@@ -35,23 +41,20 @@ module FedoraMigrate
       RDF::URI.new(ActiveFedora::Base.id_to_uri(id_component(fc3_uri)))
     end
 
+    def is_missing?(uri)
+      return false if ActiveFedora::Base.exists?(id_component(uri))
+      Logger.warn "#{source.pid} could not migrate relationship to #{uri.to_s} because it doesn't exist in Fedora4"
+      true
+    end
+
     def id_component(uri)
       uri.to_s.split(/:/).last
     end
 
-    def subject
-      @subject ||= retrieve_subject
-    end
-
-    # All the graph statements except hasModel
+    # All the graph statements except hasModel and those with missing objects
     def statements
-      graph.statements.reject { |stmt| stmt.predicate == ActiveFedora::RDF::Fcrepo::Model.hasModel }
+      graph.statements.reject { |stmt| stmt.predicate == ActiveFedora::RDF::Fcrepo::Model.hasModel || is_missing?(stmt.object) }
     end
 
-    def retrieve_subject
-      @subject = ActiveFedora::Base.find(source.pid.split(/:/).last)
-    rescue ActiveFedora::ObjectNotFoundError
-      raise FedoraMigrate::Errors::MigrationError, "Source was not found in Fedora4. Did you migrated it?"
-    end
   end
 end
